9792ef29041b6ae39ee638f7bfcfbc051724a8cb
ElopingDev <elopingbuisness@gmail.com>
2022-07-04 09:57:36 +0200

    implement racist NoRecoil

--- AssaultCubeTrainer.cpp
+++ AssaultCubeTrainer.cpp
@@ -27,19 +27,34 @@ int main()
 	//Resolve ammo n health pointer chain
 	std::vector<unsigned int> ammoOffsets = { 0x368, 0x14, 0x0 };
 	std::vector<unsigned int> healthOffsets = { 0xEC };
+	std::vector<unsigned int> recoil1Offsets = { 0x364, 0xC, 0x5E };
+	std::vector<unsigned int> recoil2Offsets = { 0x364, 0xC, 0x60 };
+
+
 	uintptr_t ammoAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddr, ammoOffsets);
 	uintptr_t healthAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddr, healthOffsets);
+	uintptr_t recoilAddr1 = FindDMAAddy(hProcess, dynamicPtrBaseAddr, recoil1Offsets);
+	uintptr_t recoilAddr2 = FindDMAAddy(hProcess, dynamicPtrBaseAddr, recoil2Offsets);
+
 
 
 	std::cout << "ammoAddr = " << "0x" << std::hex << ammoAddr << std::endl;
 	std::cout << "healthAddr = " << "0x" << std::hex << healthAddr << std::endl;
+	std::cout << "recoilAddr1 = " << "0x" << std::hex << recoilAddr1 << std::endl;
+	std::cout << "recoilAddr2 = " << "0x" << std::hex << recoilAddr2 << std::endl;
+
 
 
 	//Read ammo n health value
 	int ammoValue = 0;
 	int healthValue = 0;
+	int recoilValue1 = 0;
+	int recoilValue2 = 0;
 	ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
 	ReadProcessMemory(hProcess, (BYTE*)healthAddr, &healthValue, sizeof(healthValue), nullptr);
+	ReadProcessMemory(hProcess, (BYTE*)recoilAddr1, &recoilValue1, sizeof(recoilValue1), nullptr);
+	ReadProcessMemory(hProcess, (BYTE*)recoilAddr2, &recoilValue2, sizeof(recoilValue2), nullptr);
+
 
 	std::cout << "Current Ammo = " << std::dec << ammoValue << std::endl;
 	std::cout << "Current Health = " << std::dec << healthValue << std::endl;
@@ -48,9 +63,17 @@ int main()
 	//write to it
 	int newAmmo = 0;
 	int newHealth = 0;
+	int newRecoil1 = 0;
+	int newRecoil2 = 0;
 	std::cout << "Input new ammo" << std::endl;
 	std::cin >> newAmmo;
 	WriteProcessMemory(hProcess, (BYTE*)ammoAddr, &newAmmo, sizeof(newAmmo), nullptr);
+
+	std::cout << "we detected you were a minority, so we automatically toggled norecoil. you dont have a choice :3" << std::endl;
+	WriteProcessMemory(hProcess, (BYTE*)recoilAddr1, &newRecoil1, sizeof(newRecoil1), nullptr);
+	WriteProcessMemory(hProcess, (BYTE*)recoilAddr2, &newRecoil2, sizeof(newRecoil2), nullptr);
+
+
 
 	std::cout << "Input new health" << std::endl;
 	std::cin >> newHealth;
